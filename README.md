3a38b7670793dc13d8e0624fd7105b9bbdaade8e
Gabriel Antonin Pascoali <gabriel.296422@alunosatc.edu.br>
2024-08-20 21:49:35 -0300

    Exercício 01: Atualização

---
+++ README.md
@@ -0,0 +1,3 @@
+<h2>Gabriel Antonin Pascoali</h2>
+<p>Disciplina de Backend</p>
+<p>Portifólio de Atividades</p>
--- exercicio01/src/main/java/org/example/Main.java
+++ exercicio01/src/main/java/org/example/Main.java
@@ -5,40 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String[] filmes = {"The Walking Dead", "Zumbilândia", "Todo Mundo em Pânico", "O Hereditário"};
-
+        String[][] arrayFilmes = {
+                {"The Walking Dead", "Zumbilândia"},
+                {"Todo Mundo em Pânico", "O Hereditário"} // Dividindo em dois arrays, pra pegar o valor de cada um. O primeiro array é a categoria e o segundo é o gênero.
+        };
         Scanner leitor = new Scanner(System.in);
         System.out.println("Pergunta 1: Qual sua categoria de filme favorita?");
         System.out.println("1. Zumbis");
         System.out.println("2. Terror");
-        String resposta1 = leitor.nextLine();
-
-        String stringResultado = "Você deveria assistir";
+        Integer respostaCategoria = leitor.nextInt() - 1;
 
         System.out.println("Pergunta 2: Qual seu gênero favorito?");
         System.out.println("1. Drama");
         System.out.println("2. Comédia");
-        String resposta2 = leitor.nextLine();
+        Integer respostaGenero = leitor.nextInt() - 1;
 
-        String respostaCombinada = resposta1 + resposta2;
+        String stringResultado = "Você deveria assistir ";
+        String respostaCombinada = arrayFilmes[respostaCategoria][respostaGenero];
 
-        switch (respostaCombinada) {
-            case "12":
-                System.out.println(stringResultado + filmes[0]);
-                break;
-            case "11":
-                System.out.println(stringResultado + filmes[1]);
-                break;
-            case "21":
-                System.out.println(stringResultado + filmes[3]);
-                break;
-            case "22":
-                System.out.println(stringResultado + filmes[2]);
-                break;
-            default:
-                System.out.println("Infelizmente não encontramos nenhum filme o qual você possa assistir.");
-                break;
-        }
+        System.out.println(stringResultado + respostaCombinada);
+
 
     }
 }
